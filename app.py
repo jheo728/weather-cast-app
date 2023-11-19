ea15877f42840e66a7dd08dbdd2184f591726490
Jerry Heo <jheo728@gmail.com>
2023-11-18 17:21:52 -0800

    improved modal styling; added coords to modal title

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ def fetch_analyze_forecast():
         
         # Use the received latitude and longitude in your forecast_query function
         forecast_data = forecast_query(endpoint='points', lat=latitude, lon=longitude)
+        print(forecast_data)
         
         # Ensure that data was fetched from the API endpoint and stored in variable
         if forecast_data:
             # Limit to 5 forecast days (day/evening forecast periods)
             forecast_periods = forecast_data['properties']['periods'][:10]
-
             forecast_simplified = [{'number': x['number'], 'name': x['name'], 'temp': str(x['temperature'])+'F', 'details': x['detailedForecast'], 'img_icon': x['icon']} for x in forecast_periods]
 
             return jsonify(forecast_simplified)
@@ -36,4 +36,4 @@ def fetch_analyze_forecast():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
