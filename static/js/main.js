bb09c1684866a62ad9c9c4d642fa59fb1c69be6f
Jerry Heo <jheo728@gmail.com>
2023-11-20 17:43:50 -0800

    integrate tailwind framework

--- static/js/main.js
+++ static/js/main.js
@@ -25,6 +25,7 @@ const map = new ol.Map({
 // Add a click event handler to capture coordinates and send them to the server
 map.on("singleclick", function (event) {
   const clickedCoordinate = ol.proj.toLonLat(event.coordinate);
+
   // Check if the clicked coordinate is within the continental U.S. extent
   if (
     clickedCoordinate[0] >= continentalUSExtent[0] &&
@@ -39,6 +40,7 @@ map.on("singleclick", function (event) {
       longitude: clickedCoordinate[0],
     };
     console.log("Fetching data from weather API...");
+
     fetch(url, {
       method: "POST",
       headers: {
@@ -57,12 +59,11 @@ map.on("singleclick", function (event) {
 
           // Create a single row for forecast periods
           const row = document.createElement("div");
-          row.className = "row";
+          row.className = "flex"; // Use Tailwind CSS flex class
 
           forecastData.forEach(function (period) {
             const periodDiv = document.createElement("div");
-            // Add Bootstrap column classes as needed
-            periodDiv.className = "col-3"; // Adjust the column size as needed
+            periodDiv.className = "p-4"; // Adjust the padding as needed
 
             // Add the image
             const img = document.createElement("img");
@@ -72,27 +73,54 @@ map.on("singleclick", function (event) {
 
             // Create a div for the text content
             const textDiv = document.createElement("div");
-            textDiv.className = "forecast-text"; // Add this class
+            textDiv.className = "forecast-text text-center mt-2"; // Adjust margin-top as needed
 
             // Center align the first line of text
-            textDiv.innerHTML += `<div class="centered-text"><b>${period.name}</b></div><br>Temperature: ${period.temp}<br><br>Forecast: ${period.details}`;
+            textDiv.innerHTML += `<div class="font-bold">${period.name}</div>Temperature: ${period.temp}`;
+
+            // Add the "See Details" button
+            const seeDetailsButton = document.createElement("button");
+            seeDetailsButton.innerHTML = "See Details";
+            seeDetailsButton.className =
+              "mt-2 bg-blue-500 text-white px-2 py-1 rounded";
+            seeDetailsButton.addEventListener("click", function () {
+              // Toggle the visibility of the details
+              detailsDiv.style.display =
+                detailsDiv.style.display === "none" ? "block" : "none";
+            });
+
+            textDiv.appendChild(seeDetailsButton);
+
+            // Add a hidden div for the forecast details
+            const detailsDiv = document.createElement("div");
+            detailsDiv.innerHTML = `Forecast: ${period.details}`;
+            detailsDiv.style.display = "none"; // Initially hide the details
+            textDiv.appendChild(detailsDiv);
 
             periodDiv.appendChild(textDiv);
-            row.appendChild(periodDiv);
+
+            // Append each period vertically
+            document.getElementById("popup-content").appendChild(periodDiv);
           });
 
           // Append the row to the popup content
           popupContent.appendChild(row);
 
-          // Display the modal with updated title
-          $("#forecastModalLabel").text(
-            `Forecast Details: 
+          // Display the Tailwind CSS modal with updated title
+          document.getElementById(
+            "forecastModalLabel"
+          ).textContent = `Forecast Details: 
             ${clickedCoordinate[1].toFixed(6)}, 
-            ${clickedCoordinate[0].toFixed(6)}`
-          );
+            ${clickedCoordinate[0].toFixed(6)}`;
+          document.getElementById("forecastModal").classList.remove("hidden");
 
-          // Set the position of the popup and display it
-          $("#forecastModal").modal("show");
+          // Add event listener for the close button
+          document
+            .getElementById("closeForecastModalBtn")
+            .addEventListener("click", function () {
+              document.getElementById("forecastModal").classList.add("hidden");
+            });
+
           console.log(
             "Successfully pulled data with contents in popup window."
           );
@@ -100,18 +128,25 @@ map.on("singleclick", function (event) {
           console.log("No data available or API could not retrieve data.");
           // Display a unique popup to show no data
           const noDataContent = document.getElementById("no-data-content");
-          noDataContent.innerHTML =
-            "No data available. Try again or select another area.";
-          noDataContent.className = "no-data-content";
+          noDataContent.innerHTML = "Try again or select another area.";
+          noDataContent.classList.remove("hidden");
 
-          $("#noDataModalLabel").text(
-            `No Data Available: 
+          // Set the label for the no-data modal
+          document.getElementById(
+            "noDataModalLabel"
+          ).textContent = `No Data Available: 
             ${clickedCoordinate[1].toFixed(6)}, 
-            ${clickedCoordinate[0].toFixed(6)}`
-          );
+            ${clickedCoordinate[0].toFixed(6)}`;
 
-          // Display the Bootstrap no-data modal
-          $("#noDataModal").modal("show");
+          // Display the Tailwind CSS no-data modal
+          document.getElementById("noDataModal").classList.remove("hidden");
+
+          // Add event listener for the close button in the No Data Modal
+          document
+            .getElementById("closeNoDataModalBtn")
+            .addEventListener("click", function () {
+              document.getElementById("noDataModal").classList.add("hidden");
+            });
         }
       })
       .catch(function (error) {
